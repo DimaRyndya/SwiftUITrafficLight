e2331c880d4af5644091e94633fd238b9bd3116f
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-07-24 19:06:59 +0300

    Initial commit

---
+++ SwiftUITrafficLight/SwiftUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficLightApp.swift
+//  SwiftUITrafficLight
+//
+//  Created by Dmitriy Ryndya on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
